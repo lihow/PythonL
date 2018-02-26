84e8f3ac1158c407a0cf4d01c590d2d60c7acf99
lihow <2458858175@qq.com>
2018-02-26 22:10:05 +0800

    add 2018-2-26

---
+++ student.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+
+class Student(object):
+	def __init__(self, name, score):
+		self.name = name
+		self.score = score
+	def get_grade(self):
+		if self.score > 100 or self.score < 0:
+			raise ValueError('invalid value')
+		if self.score >= 80:
+			return 'A'
+		if self.score >= 60:
+			return 'B'
+		return 'C'		
