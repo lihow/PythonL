9e17688ca3cfb2847bf93c991d871072364a27ea
lihow <2458858175@qq.com>
2018-03-03 22:23:12 +0800

    add 2018.3.3

--- test.py
+++ test.py
@@ -1,7 +1,32 @@
 #编写一个程序，能在当前目录以及当前目录的所有子目录下查找文件名包含指定字符串的文件，并打印出相对路径
 import os
+def Print_File(files):
+    print(x for x in files)
 def Find_File(files, name):
     return [x for x in files if x.find(name) != -1]
+def Find_folder(folder,name):
+    #os.chdir(folder)
+    dirnames = [x for x in os.listdir('.') if os.path.isdir(x)]
+    #Print_File(dirnames)
+    filenames = [x for x in os.listdir('.') if os.path.isfile(x)]
+    #result_file = []
+    if len(filenames):#
+        result_file = [x for x in filenames if x.find(name) != -1]
+    #Print_File(result_file)
+    result_folder = []
+    if len(dirnames):#
+        for dirname in dirnames:
+            result_tmp = Find_folder(dirname, name)
+            result_folder.extend(result_tmp)
+        #Print_File(result_folder)
+    result_file = result_file.extend(result_folder)
+    result = []
+    for r in result_file:
+        result.append(os.path.join(folder, r))
+    #os.chdir('..')
+    return result
+        
+
 dirnames = [x for x in os.listdir('.') if os.path.isdir(x)]
 filenames = [x for x in os.listdir('.') if os.path.isfile(x)]
 for dirname in dirnames:
@@ -9,8 +34,5 @@ for dirname in dirnames:
 
 for x in Find_File(filenames, 'my'):
     print(x)
-'''
-for filename in filenames:
-    if filename.find('my') != -1:
-        print(filename)
-'''
+
+#Find_folder('.', 'my')
